c9572f7704b0d890755cc26a31eb05779c272096
gnyansoftnet <gnyan@softnet.co.in>
2026-06-04 10:59:52 +0530

    Added post request

--- src/routes/issue-petty.route.ts
+++ src/routes/issue-petty.route.ts
@@ -12,8 +12,8 @@ router.use(permissionMiddleware(CASHINFLOW_PAGE_ID));
 
 router.post("/saveUpdateDeleteIssuePetty", issuePettyController.saveUpdateDeleteIssuePetty.bind(issuePettyController));
 router.get("/getAllIssuesPettty/:orgCode/:userCode", issuePettyController.getAllIssuesPettty.bind(issuePettyController));
-router.get("/getIssuePettyByVoucherNumber", issuePettyController.getIssuePettyByVoucherNumber.bind(issuePettyController));
-router.get("/getIssuePettyPrintByVoucherNumber", issuePettyController.getIssuePettyPrintByVoucherNumber.bind(issuePettyController));
+router.post("/getIssuePettyByVoucherNumber", issuePettyController.getIssuePettyByVoucherNumber.bind(issuePettyController));
+router.post("/getIssuePettyPrintByVoucherNumber", issuePettyController.getIssuePettyPrintByVoucherNumber.bind(issuePettyController));
 router.post("/getPartyByRoutes", issuePettyController.getPartyByRoutes.bind(issuePettyController));
 
 export default router;
--- src/routes/trip-settelment.route.ts
+++ src/routes/trip-settelment.route.ts
@@ -12,7 +12,7 @@ router.use(permissionMiddleware(CASHINFLOW_PAGE_ID));
 
 router.post("/saveUpdateDeleteTripSettelment", tripSettelmentController.saveUpdateDeleteTripSettelment.bind(tripSettelmentController));
 router.get("/getAllTripSettlement/:orgCode/:userCode", tripSettelmentController.getAllTripSettlement.bind(tripSettelmentController));
-router.get("/getTripSettelmentPrintByVoucherNumber", tripSettelmentController.getTripSettelmentPrintByVoucherNumber.bind(tripSettelmentController));
-router.get("/getTripSettelmentByVoucherNumber", tripSettelmentController.getTripSettelmentByVoucherNumber.bind(tripSettelmentController));
+router.post("/getTripSettelmentPrintByVoucherNumber", tripSettelmentController.getTripSettelmentPrintByVoucherNumber.bind(tripSettelmentController));
+router.post("/getTripSettelmentByVoucherNumber", tripSettelmentController.getTripSettelmentByVoucherNumber.bind(tripSettelmentController));
 
 export default router;
